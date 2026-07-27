9b883ce0366a7841196d9198acf6df9fed643242
Rodion <rodionov.r26052006@gmail.com>
2026-07-27 21:41:45 +0300

    create project settings and db_helper for work with db async sessions

---
+++ core/models/db_helper.py
@@ -0,0 +1,25 @@
+from collections.abc import AsyncGenerator
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from core.config import settings
+
+
+class DatabaseSettings:
+    def __init__(self, db_url, db_echo):
+        self.engine = create_async_engine(
+            db_url,
+            echo=db_echo,
+        )
+        self.session_factory = async_sessionmaker(
+            bind=self.engine,
+            expire_on_commit=False,
+        )
+
+    async def session_dependency(self) -> AsyncGenerator[AsyncSession]:
+        async with self.session_factory() as session:
+            yield session
+
+
+db_helper = DatabaseSettings(
+    db_url=settings.db_url,
+    db_echo=settings.db_echo,
+)
