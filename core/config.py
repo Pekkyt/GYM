6ec5a6be5ea7fbfdb1753ef3060205f5d9a53df5
Rodion <rodionov.r26052006@gmail.com>
2026-07-27 22:10:19 +0300

    update settings

--- core/config.py
+++ core/config.py
@@ -1,8 +1,13 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
     db_url: str
     db_echo: bool
+    postgres_db: str
+    postgres_user: str
+    postgres_password: str
+
 
 settings = Settings()
